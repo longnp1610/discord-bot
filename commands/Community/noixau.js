0df5f385b4d28ab644080bd9fbd7ace3197ead08
Long Nguyen <longnp1610@gmail.com>
2023-01-11 23:23:19 +0700

    Update noixau command, replace interaction reply with channel send message

--- commands/Community/noixau.js
+++ commands/Community/noixau.js
@@ -3,16 +3,27 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("noixau")
-    .setDescription("Nói xấu người mình muốn")
+    .setDescription("Nói xấu người bạn muốn")
     .addUserOption((option) =>
-      option.setName("user").setDescription("Người bạn muốn nói xấu")
+      option
+        .setName("user")
+        .setDescription("Người bạn muốn nói")
+        .setRequired(true)
     )
     .addStringOption((option) =>
-      option.setName("string").setDescription("Lời nói xấu =)))")
+      option.setName("string").setDescription("Lời muốn nói").setRequired(true)
     ),
   async execute(interaction) {
-    const user = interaction.options.getUser("user");
-    const str = interaction.options.getString("string");
-    await interaction.reply({ content: `${user} ${str}` });
+    const { options, channel } = interaction;
+    const user = options.getUser("user");
+    const str = options.getString("string");
+
+    await interaction.reply({
+      content: "Done",
+      ephemeral: true,
+    });
+
+    await channel.send({ content: `${user} ${str}` });
+    await interaction.deleteReply();
   },
 };
